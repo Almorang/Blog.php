696a74afa37219382304bc4a7e3fc358a158dfab
Jan Horava <czechinthemail@gmail.com>
2014-05-28 18:18:59 -0700

    improved formatting

--- js/php-txtdb-blog.js
+++ js/php-txtdb-blog.js
@@ -1,23 +1,29 @@
-var db = new XMLHttpRequest();
-var post = [];
-var post_count = 0;
-var posts = [];
-
 // fetch the database file
-db.open('GET', 'db/db.txt', false);
+var db = new XMLHttpRequest();
+db.open(
+  'GET',
+  'db/db.txt',
+  false
+);
 db.send(null);
 
+
 // put blog posts into array
+var posts = [];
 posts = db.responseText.split('\n');
 
 // reverse the array to sort by date
 posts.reverse();
 
 // remove the empty "post" created by a line with only a newline
-posts.splice(0, 1);
+posts.splice(
+  0,
+  1
+);
 
-post_count = posts.length - 1;
+var post_count = posts.length - 1;
 if(post_count >= 0){
+    var post = [];
     var while_count = post_count;
     do{
         // load post into array
@@ -25,15 +31,15 @@ if(post_count >= 0){
 
         // display post
         document.getElementById('blog').innerHTML += '<div class=post>'
-            + '<b>' + post[2] + '</b>'
-            + ' by <b>' + post[1] + '</b>'
-            + ' [' + post[0] + ']<br>'
-            + post[3].split('>>').join('<br>')
-            + '</div>';
+          + '<b>' + post[2] + '</b>'
+          + ' by <b>' + post[1] + '</b>'
+          + ' [' + post[0] + ']<br>'
+          + post[3].split('>>').join('<br>')
+          + '</div>';
     }while(while_count--);
 
 }else{
     document.getElementById('blog').innerHTML = '<div class=post>'
-        + 'There are currently no blog posts to display.'
-        + '</div>';
+      + 'There are currently no blog posts to display.'
+      + '</div>';
 }
--- php/post.php
+++ php/post.php
@@ -9,37 +9,49 @@
 
         // verify length of post title
         $length = strlen(trim($_POST['title']));
-        if($length > 0 && $length <= $maxlength_title){
+        if($length > 0
+          && $length <= $maxlength_title){
 
             // verify length of post content
             $length = strlen(trim($_POST['content']));
-            if($length > 0 && $length <= $maxlength_content){
+            if($length > 0
+              && $length <= $maxlength_content){
 
                 // load the database file
-                $db = fopen('../db/db.txt', 'a+');
+                $db = fopen(
+                  '../db/db.txt',
+                  'a+'
+                );
 
                 // lock the database file
                 if(flock($db, LOCK_EX)){
 
                     // write blog post at the end of the database file
                     fwrite(
-                        $db,
-                        date('Y-m-d H:i:s')
+                      $db,
+                      date('Y-m-d H:i:s')
                       . '<'
                       . htmlspecialchars(trim($_POST['username']))
                       . '<'
                       . htmlspecialchars(trim($_POST['title']))
                       . '<'
                       . str_replace(
-                          array("\r\n", "\n", "\r"),
-                          '>>',
-                          htmlspecialchars(trim($_POST['content']))
+                        array(
+                          "\r\n",
+                          "\n",
+                          "\r"
+                        ),
+                        '>>',
+                        htmlspecialchars(trim($_POST['content']))
                       )
                       . "\n"
                     );
 
                     // unlock the database file
-                    flock($db, LOCK_UN);
+                    flock(
+                      $db,
+                      LOCK_UN
+                    );
 
                     // close the database file
                     fclose($db);

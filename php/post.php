2943b78ef88632210437fec77f85dd0c985256ad
Jan Horava <czechinthemail@gmail.com>
2014-07-05 11:42:15 -0700

    removed nested if statements, added error messages

--- php/post.php
+++ php/post.php
@@ -5,59 +5,65 @@
     require 'settings.php';
 
     // verify password
-    if($_POST['password'] == $secret_password){
+    if($_POST['password'] != $secret_password){
+        header('Location:../?incorrect_password');
+        exit;
+    }
 
-        // verify length of post title
-        $length = strlen(trim($_POST['title']));
-        if($length > 0
-          && $length <= $maxlength_title){
+    // verify length of post title
+    $length = strlen(trim($_POST['title']));
+    if($length < 1
+      || $length > $maxlength_title){
+        header('Location:../?invalid_title_length');
+        exit;
+    }
 
-            // verify length of post content
-            $length = strlen(trim($_POST['content']));
-            if($length > 0
-              && $length <= $maxlength_content){
+    // verify length of post content
+    $length = strlen(trim($_POST['content']));
+    if($length < 1
+      && $length > $maxlength_content){
+        header('Location:../?invalid_content_length');
+        exit;
+    }
 
-                // load the database file
-                $db = fopen(
-                  $database_path,
-                  'a+'
-                );
+    // load the database file
+    $db = fopen(
+      $database_path,
+      'a+'
+    );
 
-                // lock the database file
-                if(flock($db, LOCK_EX)){
+    // lock the database file
+    if(flock($db, LOCK_EX)){
 
-                    // write blog post at the end of the database file
-                    fwrite(
-                      $db,
-                      date('Y-m-d H:i:s')
-                      . '<'
-                      . htmlspecialchars(trim($_POST['username']))
-                      . '<'
-                      . htmlspecialchars(trim($_POST['title']))
-                      . '<'
-                      . str_replace(
-                        array(
-                          "\r\n",
-                          "\n",
-                          "\r"
-                        ),
-                        '>>',
-                        htmlspecialchars(trim($_POST['content']))
-                      )
-                      . "\n"
-                    );
+        // write blog post at the end of the database file
+        fwrite(
+          $db,
+          date('Y-m-d H:i:s')
+          . '<'
+          . htmlspecialchars(trim($_POST['username']))
+          . '<'
+          . htmlspecialchars(trim($_POST['title']))
+          . '<'
+          . str_replace(
+            array(
+              "\r\n",
+              "\n",
+              "\r"
+            ),
+            '>>',
+            htmlspecialchars(trim($_POST['content']))
+          )
+          . "\n"
+        );
 
-                    // unlock the database file
-                    flock(
-                      $db,
-                      LOCK_UN
-                    );
+        // unlock the database file
+        flock(
+          $db,
+          LOCK_UN
+        );
 
-                    // close the database file
-                    fclose($db);
-                }
-            }
-        }
+        // close the database file
+        fclose($db);
     }
 }
 

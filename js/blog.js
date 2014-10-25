6d9dcd8fbb82821a2a73f0f392eced21a8034895
Jan Horava <czechinthemail@gmail.com>
2014-10-25 13:00:56 -0700

    improved formatting and clarity

--- js/blog.js
+++ js/blog.js
@@ -1,5 +1,5 @@
 ;(function(){
-    // fetch the database file
+    // Fetch the database file.
     var db = new XMLHttpRequest();
     db.open(
       'GET',
@@ -8,40 +8,42 @@
     );
     db.send(null);
 
-    // put blog posts into array
+    // Put blog posts into an array.
     var posts = [];
     posts = db.responseText.split('\n');
 
-    // reverse the array to sort by date
+    // Reverse the array to sort by date.
     posts.reverse();
 
-    // remove the empty "post" created by a line with only a newline
+    // Remove the empty "post" created by a line with only a newline.
     posts.splice(
       0,
       1
     );
 
     var post_count = posts.length - 1;
-    if(post_count >= 0){
-        var post = [];
 
-        var while_count = post_count;
-        do{
-            // load post into array
-            post = posts[post_count - while_count].split('<');
-
-            // display post
-            document.getElementById('blog').innerHTML += '<div class=blog-post>'
-              + '<b>' + post[2] + '</b>'
-              + ' by <b>' + post[1] + '</b>'
-              + ' [' + post[0] + ']<br>'
-              + post[3].split('>>').join('<br>')
-              + '</div>';
-        }while(while_count--);
-
-    }else{
+    // No posts? We're done here.
+    if(post_count < 0){
         document.getElementById('blog').innerHTML = '<div class=blog-post>'
           + 'There are currently no blog posts to display.'
           + '</div>';
+        return;
     }
+
+    var post = [];
+
+    var loop_counter = post_count;
+    do{
+        // Load post into array.
+        post = posts[post_count - loop_counter].split('<');
+
+        // Display post.
+        document.getElementById('blog').innerHTML += '<div class=blog-post>'
+          + '<b>' + post[2] + '</b>'
+          + ' by <b>' + post[1] + '</b>'
+          + ' [' + post[0] + ']<br>'
+          + post[3].split('>>').join('<br>')
+          + '</div>';
+    }while(loop_counter--);
 }());

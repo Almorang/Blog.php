9bccb8321d9281096ba9c96a0fbaf808624001f0
Jan Horava <czechinthemail@gmail.com>
2015-02-15 18:29:21 -0800

    added delete checkbox

--- php/post.php
+++ php/post.php
@@ -9,25 +9,33 @@
         exit;
     }
 
-    // Create an array with new post information.
-    $post = array(
-      "content" => htmlspecialchars(trim($_POST['content'])),
-      "date" => date('Y-m-d H:i:s'),
-      "title" => htmlspecialchars(trim($_POST['title'])),
-      "username" => htmlspecialchars(trim($_POST['username'])),
-    );
     $post_id = htmlspecialchars(trim($_POST['id']));
 
-    // Add the new $post to the existing posts
-    //   and sort the posts by date.
+    // Fetch saved posts.
     $posts = json_decode(file_get_contents($database_path), true);
-    $posts[$post_id] = $post;
-    uasort($posts, function($a, $b){
-        return strtotime($b['date']) - strtotime($a['date']);
-    });
-    $posts = json_encode($posts, JSON_FORCE_OBJECT);
 
-    // Update the database file.
+    // Delete post if requested.
+    if($_POST['delete'] == 1){
+        unset($posts[$post_id]);
+
+    // Else save the post.
+    }else{
+        $post = array(
+          "content" => htmlspecialchars(trim($_POST['content'])),
+          "date" => date('Y-m-d H:i:s'),
+          "title" => htmlspecialchars(trim($_POST['title'])),
+          "username" => htmlspecialchars(trim($_POST['username'])),
+        );
+        $posts[$post_id] = $post;
+
+        // Sort saved posts by date.
+        uasort($posts, function($a, $b){
+            return strtotime($b['date']) - strtotime($a['date']);
+        });
+    }
+
+    // Save all posts to the database file.
+    $posts = json_encode($posts, JSON_FORCE_OBJECT);
     file_put_contents(
       $database_path,
       $posts

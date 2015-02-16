cf17711c5d918e08102d927a8e492418b50f32c5
Jan Horava <czechinthemail@gmail.com>
2015-02-15 17:49:44 -0800

    moved posting form to separate page, added post ids

---
+++ js/post.js
@@ -0,0 +1,28 @@
+;(function(){
+    // Check if a post is being edited.
+    var post_id = window.location.search.substring(1);
+    if(post_id.length <= 0){
+        return;
+    }
+
+    document.getElementById('id').value = post_id;
+
+    // Fetch the database file.
+    var db = new XMLHttpRequest();
+    db.open(
+      'GET',
+      'db/db.json',
+      false
+    );
+    db.send(null);
+
+    // Fetch posts.
+    var posts = JSON.parse(db.responseText);
+
+    // Use existing post data,
+    //   if any exists.
+    if(post_id in posts){
+        document.getElementById('content').value = posts[post_id]['content'];
+        document.getElementById('title').value = posts[post_id]['title'];
+    }
+}());
--- php/post.php
+++ php/post.php
@@ -1,6 +1,5 @@
 <?php ?>
 <?php if(isset($_POST['post-button'])){
-
     // Load settings.
     require 'settings.php';
 
@@ -17,13 +16,15 @@
       "title" => htmlspecialchars(trim($_POST['title'])),
       "username" => htmlspecialchars(trim($_POST['username'])),
     );
+    $post_id = htmlspecialchars(trim($_POST['id']));
 
-    // Add the new $post to the existing posts.
+    // Add the new $post to the existing posts
+    //   and sort the posts by date.
     $posts = json_decode(file_get_contents($database_path), true);
-    array_unshift(
-      $posts,
-      $post
-    );
+    $posts[$post_id] = $post;
+    uasort($posts, function($a, $b){
+        return strtotime($b['date']) - strtotime($a['date']);
+    });
     $posts = json_encode($posts, JSON_FORCE_OBJECT);
 
     // Update the database file.
